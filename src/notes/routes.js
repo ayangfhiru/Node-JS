7c8259b1f66f181a41736040a8acca864b059812
fhiru <fhiruayang@gmail.com>
2022-02-18 23:57:24 +0700

    push keempat

--- src/notes/routes.js
+++ src/notes/routes.js
@@ -1,35 +1,90 @@
 const express = require('express');
+const { schema } = require('../validation');
+const validateNote = require('../validation');
 const NotesServices = require('./services');
 const route = express.Router();
 
 route.post('/notes', async(req, res)=>{
-    const {title, body} =  req.body;
-    const notes = await new NotesServices().addNotes({title, body})
-    res.json(notes)
+    try {
+        await validateNote.validateAsync(req.body)
+        const notes = await new NotesServices().addNotes(req.body);
+        res.json({
+            status: 'Success',
+            message: 'Note create successfully',
+            data: notes
+        });
+    } catch (error) {
+        res.json({
+            status: 'Fail',
+            message: error.message
+        })
+    }
 });
 
 route.get('/notes', async(req, res)=>{
-    const notes = await new NotesServices().getNotes();
-    res.json(notes);
+    try {
+        const notes = await new NotesServices().getNotes();
+        res.json({
+            status: 'Success',
+            message: 'Data notes',
+            data: { notes }
+        });
+    } catch (error) {
+        res.json({
+            status: 'Fail',
+            message: error.message
+        })
+    }
 });
 
 route.get('/notes/:id', async(req, res)=>{
-    const notesId =  req.params.id;
-    const getNotesById = await new NotesServices().getNotesById(notesId);
-    res.json(getNotesById);
+    try {
+     const notesId = req.params.id;
+     const getNotesById = await new NotesServices().getNotesById(notesId);
+     res.json({
+         status: 'Success',
+         data: getNotesById
+     });   
+    } catch (error) {
+        res.json({
+            status: 'Fail',
+            message: error.message
+        })
+    }
 });
 
 route.put('/notes/:id', async(req, res)=>{
-    const noteId = req.params.id;
-    const {title, body} = req.body;
-    const editNote = await new NotesServices().editNotesById(noteId, {title, body});
-    res.json(editNote);
+    try {
+        await validateNote.validateAsync(req.body)
+        const noteId = req.params.id;
+        // const { title, body } = req.body;
+        const editNote = await new NotesServices().editNotesById(noteId, req.body);
+        res.json({
+            status: 'Success',
+            data: editNote
+        });
+    } catch (error) {
+        res.json({
+            status: 'Fail',
+            message: error.message
+        })
+    }
 })
 
 route.delete('/notes/:id', async(req, res)=>{
-    const noteId = req.params.id;
-    const deleteNote = await new NotesServices().deleteNotesById(noteId);
-    res.json(deleteNote);
+    try {
+        const noteId = req.params.id;
+        const deleteNote = await new NotesServices().deleteNotesById(noteId);
+        res.json({
+            status: `Success menghapus Id ${noteId}`,
+            message: deleteNote
+        });
+    } catch (error) {
+        res.json({
+            status: 'Fail',
+            message: error.message
+        })
+    }
 })
 
 module.exports = route;
